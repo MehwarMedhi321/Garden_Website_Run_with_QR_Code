383fc8e7e8b38212d3cd35d6f4256295c5267dbb
Mehwar Medhi <mehwarmedhi@gmail.com>
2024-03-14 02:22:09 +0530

    Add some new Pages

--- README.md
+++
@@ -1,4 +0,0 @@
-![image](https://github.com/MehwarMedhi321/Garden_Website_Run_with_QR_Code/assets/154052609/74a2a0f7-2c9f-45db-964e-76d2871f74ac)
-![image](https://github.com/MehwarMedhi321/Garden_Website_Run_with_QR_Code/assets/154052609/9c453f01-a7e2-4108-828b-436908e2cc9b)
-![image](https://github.com/MehwarMedhi321/Garden_Website_Run_with_QR_Code/assets/154052609/ba1d86e1-d366-436e-97b9-ccb9ab54d793)
-![Uploading image.png…]()
--- script.js
+++ script.js
@@ -2,13 +2,19 @@ let firstNumber = document.getElementById('firstNumber');
 let SecondNumber = document.getElementById('SecondNumber');
 let copyIconOne = document.getElementById('copyIconOne');
 let copyIconTwo = document.getElementById('copyIconTwo');
+
+
 let newicon1 = document.getElementById('newicon1');
 let newicon2 = document.getElementById('newicon2');
+let FormContent = document.getElementById('FormContent');
+let BtnLease = document.getElementById('BtnLease');
+let BtnContact = document.getElementById('BtnContact');
+let toggle = document.getElementById('toggle');
 
 
 
-copyIconOne.addEventListener('click', function(){
-    if(firstNumber.value != "" || firstNumber.value.length >= 1){
+copyIconOne.addEventListener('click', function () {
+    if (firstNumber.value != "" || firstNumber.value.length >= 1) {
         navigator.clipboard.writeText(firstNumber.value);
         copyIconOne.style.display = "none";
         newicon1.innerHTML = "✔";
@@ -16,15 +22,14 @@ copyIconOne.addEventListener('click', function(){
         setInterval(() => {
             copyIconOne.style.display = "inline";
             newicon1.innerHTML = "";
-            
-        }, 6000);
+
+        }, 9000);
 
     }
 
 })
-
-copyIconTwo.addEventListener('click', function(){
-    if(SecondNumber.value != "" || SecondNumber.value.length >= 1){
+copyIconTwo.addEventListener('click', function () {
+    if (SecondNumber.value != "" || SecondNumber.value.length >= 1) {
         navigator.clipboard.writeText(SecondNumber.value);
         copyIconTwo.style.display = "none";
         newicon2.innerHTML = "✔";
@@ -32,13 +37,31 @@ copyIconTwo.addEventListener('click', function(){
         setInterval(() => {
             copyIconTwo.style.display = "inline";
             newicon2.innerHTML = "";
-            
-        }, 3000);
+
+        }, 9000);
 
     }
 
 })
 
-  
-  
-  
+
+
+
+
+// Form open and hide
+
+BtnLease.addEventListener('click', function () {
+    FormContent.style.display="inline"
+})
+
+BtnContact.addEventListener('click', function () {
+    FormContent.style.display="inline"
+})
+
+// Toggle button
+function ShowUrdu() {
+    window.open("file:///D:/ProjectGarden/Urdu.html","_self");
+}
+function ShowEnglish() {
+    window.open("file:///D:/ProjectGarden/index.html","_self");
+}
